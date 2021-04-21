600d8c72be9043ac0791176d9090dfd391860bca
Aditya Sharma <adityasharma1709@gmail.com>
2021-04-20 18:58:39 -0700

    All updates

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,277 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'onboarding_page.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
 
 void main() {
   runApp(MyApp());
 }
+
+// final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          body: Container(
+            child: MyHomePage(),
+          ),
+        ));
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _pageState = 0;
+  var backgroundColor = Colors.white;
+  var headingColor = Color(0xFF3a82f7);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  double loginYOffset = 0;
+  double windowWidth = 0;
+  double windowHeight = 0;
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    windowHeight = MediaQuery.of(context).size.height;
+    windowWidth = MediaQuery.of(context).size.width;
+    switch (_pageState) {
+      case 0:
+        backgroundColor = Colors.white;
+        headingColor = Color(0xFF3a82f7);
+        loginYOffset = windowHeight;
+        break;
+      case 1:
+        backgroundColor = Color(0xFFfcba3e);
+        headingColor = Colors.white;
+        loginYOffset = 270;
+        break;
+    }
+    return Stack(
+      children: <Widget>[
+        AnimatedContainer(
+          curve: Curves.fastLinearToSlowEaseIn,
+          duration: Duration(milliseconds: 1000),
+          color: backgroundColor,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _pageState = 0;
+                  });
+                },
+                child: Column(
+                  children: <Widget>[
+                    Container(
+                      margin: const EdgeInsets.only(top: 100),
+                      child: Text(
+                        "Mindful Curiosity",
+                        style: TextStyle(color: headingColor, fontSize: 28),
+                      ),
+                    ),
+                    // Lottie.asset('assets/cat.json')
+                  ],
+                ),
+              ),
+              // Lottie.asset('assets/cat.json'),
+              Container(
+                child: Center(
+                  child: Text(""),
+                ),
+              ),
+              Container(
+                child: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      if (_pageState != 0) {
+                        _pageState = 0;
+                      } else {
+                        _pageState = 1;
+                      }
+                    });
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(50),
+                    padding: EdgeInsets.all(20),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        color: Color(0xFF3a82f7),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Center(
+                      child: Text(
+                        "Get Started",
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+        GestureDetector(
+          onTap: () {
+            setState(() {
+              _pageState = 0;
+              debugPrint("HI");
+            });
+          },
+          child: AnimatedContainer(
+            padding: EdgeInsets.all(20),
+            curve: Curves.fastLinearToSlowEaseIn,
+            duration: Duration(milliseconds: 1000),
+            transform: Matrix4.translationValues(0, loginYOffset, 1),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(25),
+                    topRight: Radius.circular(25))),
+            child: Column(
+              children: <Widget>[
+                // Lottie.asset('assets/cat.json'),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20),
+                  child: Text(
+                    "Enter Your User ID",
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.only(bottom: 20, left: 30, right: 30),
+                  child: Text(
+                    "This is the code you were given at orientation. Letters are case sensitive.",
+                    style: TextStyle(fontSize: 18),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                InputWithIcon(
+                  icon: Icons.vpn_key,
+                  hint: "Enter User ID...",
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    print('HI');
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => OnboardingPage()),
+                    );
+                    //final snackbar = SnackBar(content: Text("HI"));
+                    // ScaffoldMessenger.of(context).showSnackBar(snackbar);
+                  },
+                  child: Column(children: <Widget>[
+                    PrimaryButton(btnText: "Login"),
+                  ]),
+                  // PrimaryButton(btnText: "Login"),
+                ),
+                // PrimaryButton(btnText: "Login"),
+                // OutlineBtn(btnText: "Create an Account")
+                Lottie.asset('assets/cat.json')
+              ],
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class InputWithIcon extends StatefulWidget {
+  final IconData icon;
+  final String hint;
+  InputWithIcon({this.icon, this.hint});
+
+  @override
+  _InputWithIconState createState() => _InputWithIconState();
+}
+
+class _InputWithIconState extends State<InputWithIcon> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xFFBC7C7C7), width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      child: Row(
+        children: <Widget>[
+          Container(
+              width: 60,
+              child: Icon(
+                widget.icon,
+                size: 20,
+                color: Color(0xFFBB9B9B9),
+              )),
+          Expanded(
+            child: TextField(
+              decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(vertical: 20),
+                  border: InputBorder.none,
+                  hintText: widget.hint),
+            ),
+          )
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    );
+  }
+}
+
+class PrimaryButton extends StatefulWidget {
+  final String btnText;
+  PrimaryButton({this.btnText});
+
+  @override
+  _PrimaryButtonState createState() => _PrimaryButtonState();
+}
+
+class _PrimaryButtonState extends State<PrimaryButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Color(0xFF3a82f7), borderRadius: BorderRadius.circular(50)),
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(color: Colors.white, fontSize: 16),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class OutlineBtn extends StatefulWidget {
+  final String btnText;
+  OutlineBtn({this.btnText});
+
+  @override
+  _OutlineBtnState createState() => _OutlineBtnState();
+}
+
+class _OutlineBtnState extends State<OutlineBtn> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          border: Border.all(color: Color(0xFF3a82f7), width: 2),
+          borderRadius: BorderRadius.circular(50)),
+      padding: EdgeInsets.all(20),
+      child: Center(
+        child: Text(
+          widget.btnText,
+          style: TextStyle(color: Color(0xFF3a82f7), fontSize: 16),
+        ),
+      ),
     );
   }
 }
---
+++ lib/onboarding_page.dart
@@ -0,0 +1,79 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class OnboardingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        // title: Text('Onboarding'),
+        backgroundColor: Colors.amber,
+      ),
+      body: Container(
+        color: Colors.amber,
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Welcome to Curiosity',
+              style: TextStyle(color: Colors.white),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: CurvedNavigationBar(
+        color: Colors.white,
+        backgroundColor: Colors.amber,
+        height: 65,
+        items: <Widget>[
+          Icon(Icons.home, size: 20, color: Colors.black),
+          Icon(Icons.today, size: 20, color: Colors.black),
+          Icon(Icons.list, size: 20, color: Colors.black),
+        ],
+        animationDuration: Duration(milliseconds: 200),
+        animationCurve: Curves.bounceInOut,
+        onTap: (index) {
+          // debugPrint("Current index is $index");
+        },
+      ),
+    );
+    //   body: PageView.builder(
+    //       itemCount: 3,
+    //       itemBuilder: (_, i) {
+    //         return Padding(
+    //           padding: const EdgeInsets.all(40),
+    //           child: Column(
+    //             children: [
+    //               SvgPicture.asset(
+    //                 "assets/delivery.svg",
+    //                 height: 300,
+    //               ),
+    //               Text(
+    //                 "Curiosity",
+    //                 style: TextStyle(
+    //                   fontSize: 35,
+    //                   fontWeight: FontWeight.bold,
+    //                 ),
+    //               ),
+    //               SizedBox(
+    //                 height: 28,
+    //               ),
+    //               Text(
+    //                 "Lorem ipsum",
+    //                 textAlign: TextAlign.center,
+    //                 style: TextStyle(
+    //                     fontSize: 18,
+    //                     color: CupertinoColors.darkBackgroundGray),
+    //               )
+    //             ],
+    //           ),
+    //         );
+    //       }),
+    // );
+  }
+}
