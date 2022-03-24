852f15c2564f49d7d765500366432ca5dec4ce7a
vince nguyen <vincehnguyen@ucsb.edu>
2022-03-24 11:30:23 -0700

    Connected task_carousel to its introduction screen

--- lib/main.dart
+++ lib/main.dart
@@ -285,7 +285,7 @@ class _MyHomePageState extends State<MyHomePage> {
           Navigator.pushReplacementNamed(
             context,
             // '/study_id',
-            '/good_morning',
+            '/introduction_daily_challenge',
           );
         }
       }
