bd4260bed762fd91b7d706037162377d5d18e7a9
vince nguyen <vincehnguyen@ucsb.edu>
2022-04-04 13:15:38 -0700

    Added more minfulness audio

--- lib/screens/play_audio_screen.dart
+++ lib/screens/play_audio_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:percent_indicator/percent_indicator.dart';
 import './mindful_completion_screen.dart';
+import 'dart:math';
 
 class AudioPlayer extends StatefulWidget {
   const AudioPlayer({Key key}) : super(key: key);
@@ -24,18 +25,37 @@ class _AudioPlayerState extends State<AudioPlayer> {
   @override
   void didChangeDependencies() {
     final args = ModalRoute.of(context).settings.arguments as int;
+
+    Random random = new Random();
     //Set the image and audio based on the setting selected.
     if (args == 0) {
+      //Picks a random number in range [0,2]
+      int randomNumber = random.nextInt(3);
+      List<String> audioChoices = [
+        'assets/audio/eating1.mp3',
+        'assets/audio/eating2.mp3',
+        'assets/audio/eating3.mp3'
+      ];
+
       image = 'assets/images/food.jpg';
-      audio = 'assets/audio/beach.mp3';
+      audio = audioChoices[randomNumber];
       print("0");
+      print(randomNumber);
     } else if (args == 1) {
       image = 'assets/images/forest.jpeg';
-      audio = 'assets/audio/forest.mp3';
+      audio = 'assets/audio/walking1.mp3';
       print("1");
     } else {
+      //Picks a random number in range [0,2]
+      int randomNumber = random.nextInt(3);
+      List<String> audioChoices = [
+        'assets/audio/washing1.mp3',
+        'assets/audio/washing2.mp3',
+        'assets/audio/washing3.mp3'
+      ];
+
       image = 'assets/images/beach.jpg';
-      audio = 'assets/audio/beach.mp3';
+      audio = audioChoices[randomNumber];
       print("2");
     }
     setUpPlaylist(audio);
